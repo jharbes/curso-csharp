4394761438c690eb0b3d91221dc10d3a5de4377a
Jorge Harbes <jharbes@hotmail.com>
2022-11-15 09:07:41 -0300

    programa WindowsEx000 parte 3

--- exercicios/WindowsEx000/Form1.Designer.cs
+++ exercicios/WindowsEx000/Form1.Designer.cs
@@ -28,21 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn = new System.Windows.Forms.Button();
+            this.LblMsg = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn.Location = new System.Drawing.Point(279, 268);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(262, 101);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "Clique em mim!";
+            this.btn.UseVisualStyleBackColor = true;
+            // 
+            // LblMsg
+            // 
+            this.LblMsg.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LblMsg.Location = new System.Drawing.Point(44, 30);
+            this.LblMsg.Name = "LblMsg";
+            this.LblMsg.Size = new System.Drawing.Size(707, 106);
+            this.LblMsg.TabIndex = 1;
+            this.LblMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblMsg);
+            this.Controls.Add(this.btn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Primeiro Programa C#";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Button btn;
+        private Label LblMsg;
     }
 }
