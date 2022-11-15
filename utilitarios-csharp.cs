d76cd134fcb95780d68cc9ee687818b77fb17520
Jorge Harbes <jharbes@hotmail.com>
2022-11-15 07:51:55 -0300

    Create utilitarios-csharp.cs

---
+++ utilitarios-csharp.cs
@@ -0,0 +1,16 @@
+System.Console.Out.WriteLine("mensagem"); // Imprimir em console
+
+System.Console.ReadKey(); // Comando para ficar aguardando aperto de tecla para finalizar programa
+
+/*
+
+Como importamos a biblioteca System podemos suprimir esse comando nas opções acima
+ficando apenas Console.Out.WriteLine("mensagem");
+ou até mesmo suprimir o Out ficando apenas Console.WriteLine("mensagem"); e Console.ReadKey();
+
+se nós adicionarmos a biblioteca 
+using static System.Console;
+podemos suprimir tudo ficando apenas WriteLine("mensagem");
+e ReadKey();
+
+*/
