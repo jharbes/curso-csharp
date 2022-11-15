e1077fcfbad286f4a2ef3b1e06d750bda6346801
Jorge Harbes <jharbes@hotmail.com>
2022-11-15 08:24:21 -0300

    WindowsEx000 atualizacao

--- exercicios/WindowsEx000/Form1.Designer.cs
+++ exercicios/WindowsEx000/Form1.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Primeiro Programa C#";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
