e1ffec144be9b2126fc12edb7adc77a5c0d14acc
Jorge Harbes <jharbes@hotmail.com>
2022-11-15 08:25:51 -0300

    WindowsEx000 parte 3

--- exercicios/WindowsEx000/Form1.Designer.cs
+++ exercicios/WindowsEx000/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Primeiro Programa C#";
             this.ResumeLayout(false);
