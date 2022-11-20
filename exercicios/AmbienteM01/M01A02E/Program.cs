8a119c10d7cf956957307f316b24b2a02b98d7c6
Jorge Harbes <jharbes@hotmail.com>
2022-11-20 09:47:35 -0300

    Fim Modulo 01 Aula 02

--- exercicios/AmbienteM01/M01A02E/Program.cs
+++ exercicios/AmbienteM01/M01A02E/Program.cs
@@ -1,9 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+Console.SetCursorPosition(20, 5); // Altera a posicao do cursor
+
+Console.ForegroundColor = ConsoleColor.DarkGray; // Escolhe a cor da letra
+
+Console.BackgroundColor = ConsoleColor.Yellow; // Escolhe a cor de fundo
+
 Console.WriteLine("Olá querido(a) Estudonauta!");
 Console.ReadKey();
 
+Console.ResetColor(); // Reseta as cores utilizadas no terminal
+
 Console.Clear();
 
+Console.SetCursorPosition(10, 3); // Altera a posicao do cursor
+
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.BackgroundColor = ConsoleColor.Blue;
+
 Console.WriteLine("Tudo bem com você?");
+
+Console.ResetColor();
 Console.ReadKey();
 
--- utilitarios-csharp.cs
+++ utilitarios-csharp.cs
@@ -5,6 +5,14 @@ System.Console.ReadKey(); // Comando para ficar aguardando aperto de tecla para 
 
 System.Console.Clear(); // Limpa a tela de console
 
+System.Console.SetCursorPosition(20, 5); // Altera a posicao do cursor
+
+System.Console.ForegroundColor = ConsoleColor.DarkGray; // Escolhe a cor da letra
+
+System.Console.BackgroundColor = ConsoleColor.Yellow; // Escolhe a cor de fundo
+
+System.Console.ResetColor(); // Reseta as cores utilizadas no terminal
+
 /*
 
 Como importamos a biblioteca System podemos suprimir esse comando nas opções acima
