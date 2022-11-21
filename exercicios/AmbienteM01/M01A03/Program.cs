8636b9be57286557fc9c8a314473bbdc40a66751
Jorge Harbes <jharbes@hotmail.com>
2022-11-21 10:44:20 -0300

    Fim Módulo 01 Aula 03

--- exercicios/AmbienteM01/M01A03/Program.cs
+++ exercicios/AmbienteM01/M01A03/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
+
+/* AULA DE COMENTÁRIOS, TIPOS DE COMENTÁRIOS */
+
+// Comentário de uma linha
+
+/*
+ * 
+ * Comentário de multiplas linhas
+ * 
+ */
+
+/*
+
+Comentário de multiplas linhas
+
+*/
