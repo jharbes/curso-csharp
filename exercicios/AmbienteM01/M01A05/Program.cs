f000bdf4cca8a4780e1fd737aff777b3db4e821d
Jorge Harbes <jharbes@hotmail.com>
2022-11-23 05:32:57 -0300

    Inicio Modulo 01 Aula 05 B

--- exercicios/AmbienteM01/M01A05/Program.cs
+++ exercicios/AmbienteM01/M01A05/Program.cs
@@ -1,5 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var n = 0;
+var n = 0; // Quando usamos o var o proprio sistema define qual tipo será nossa variável
+           // o que as vezes pode nao ser a melhor escolha
 
 Console.WriteLine("A variável n tem "+n);
+Console.WriteLine("n é to tipo "+n.GetType().Name);
+
+
+byte x= 0; // Ocupa menos memória
+Console.WriteLine("\nx é do tipo "+x.GetType().Name);
+
+
+var nome = "Jorge";
+Console.WriteLine("\nA variável nome tem "+nome);
+Console.WriteLine("nome é do tipo "+nome.GetType().Name);
