ffdf41353c4569fbd472ceb60a602145f96b14cd
Jorge Harbes <jharbes@hotmail.com>
2022-11-24 12:54:08 -0300

    Fim Modulo 01 Aula06

--- exercicios/AmbienteM01/M01A06/Program.cs
+++ exercicios/AmbienteM01/M01A06/Program.cs
@@ -1,8 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-// DECLARANDO CONSTANTES - identico a declaracao de variaveis, mas com a palavra const na frente
+// DECLARANDO CONSTANTES - identico a declaracao de variaveis, mas com a palavra const na frente, o tipo da constante deve ser referenciado, diferente da declaracao de variavel que aceita apenas o var.
+
+// É UMA PRÁTICA COMUM nomear as constantes com letras maiusculas
 
 const string escola = "Estudonauta";
 
 Console.WriteLine("Eu estudo na escola "+escola);
+
+
