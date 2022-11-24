052674176e5132d4b3ef096ddda6e4a9eee1f8ed
Jorge Harbes <jharbes@hotmail.com>
2022-11-24 12:47:47 -0300

    inicio Modulo 01 Aula 06

---
+++ exercicios/AmbienteM01/M01A06/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// DECLARANDO CONSTANTES - identico a declaracao de variaveis, mas com a palavra const na frente
+
+const string escola = "Estudonauta";
+
+Console.WriteLine("Eu estudo na escola "+escola);
