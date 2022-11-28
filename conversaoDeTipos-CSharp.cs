950dbce56f7addb31776b3c52b507d679447ddbc
Jorge Harbes <jharbes@hotmail.com>
2022-11-28 09:17:34 -0300

    modulo 01 aula 07 commit 02

--- conversaoDeTipos-CSharp.cs
+++ conversaoDeTipos-CSharp.cs
@@ -9,4 +9,32 @@
 * Conversao explicita (utilizando o typecast)
 *
 * Conversao por classes Auxiliares
+*/
 
+// *** CONVERSAO IMPLICITA
+
+// Conversao implicita int -> float
+
+int a=8;
+float b = a;
+Console.WriteLine("O valor de a é "+a+" do tipo "+a.GetType().Name);
+Console.WriteLine("O valor de b é "+b+" do tipo "+b.GetType().Name);
+
+
+// Conversao implicita float -> int, nesse caso a conversao implicita nao funciona, precisaremos utilizar a conversao explicita
+
+/*
+float c = 8;
+int d = c;
+Console.WriteLine("O valor de c é " + c + " do tipo " + c.GetType().Name);
+Console.WriteLine("O valor de d é " + d + " do tipo " + d.GetType().Name);
+*/
+
+
+// Conversao explicita float -> int, nesse caso a conversao implicita nao funciona, precisaremos utilizar a conversao explicita
+
+
+float c = 8;
+int d = (int)c;
+Console.WriteLine("O valor de c é " + c + " do tipo " + c.GetType().Name);
+Console.WriteLine("O valor de d é " + d + " do tipo " + d.GetType().Name);
--- exercicios/AmbienteM01/M01A07/Program.cs
+++ exercicios/AmbienteM01/M01A07/Program.cs
@@ -1,2 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+*  CONVERSÃO DE TIPOS EM C#
+*/
+
+// TIPOS DE CONVERSAO
+
+/* Conversao implicita
+*
+* Conversao explicita (utilizando o typecast)
+*
+* Conversao por classes Auxiliares
+*/
+
+
+// *** CONVERSAO IMPLICITA
+
+// Conversao implicita int -> float
+
+int a=8;
+float b = a;
+Console.WriteLine("O valor de a é "+a+" do tipo "+a.GetType().Name);
+Console.WriteLine("O valor de b é "+b+" do tipo "+b.GetType().Name);
+
+
+// Conversao implicita float -> int, nesse caso a conversao implicita nao funciona, precisaremos utilizar a conversao explicita
+
+/*
+float c = 8f;
+int d = c;
+Console.WriteLine("O valor de c é " + c + " do tipo " + c.GetType().Name);
+Console.WriteLine("O valor de d é " + d + " do tipo " + d.GetType().Name);
+*/
+
+
+
+
+// Conversao explicita float -> int, nesse caso a conversao implicita nao funciona, precisaremos utilizar a conversao explicita
+
+
+float c = 8;
+int d = (int)c;
+Console.WriteLine("\nO valor de c é " + c + " do tipo " + c.GetType().Name);
+Console.WriteLine("O valor de d é " + d + " do tipo " + d.GetType().Name);
