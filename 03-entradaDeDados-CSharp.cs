bfd38e2b846945e405df5490427d01d3813dde81
Jorge Harbes <jharbes@hotmail.com>
2022-12-04 16:41:39 -0300

    modulo 01 aula 08 C fim

--- 03-entradaDeDados-CSharp.cs
+++ 03-entradaDeDados-CSharp.cs
@@ -7,3 +7,11 @@ Console.Write("Qual é o seu nome? ");
 nome = Console.ReadLine(); // Comando para ler uma linha em string no teclado
 
 Console.WriteLine("Muito prazer em te conhecer "+nome);
+
+// LENDO NÚMEROS PELO TECLADO EM C#
+
+int n=Convert.ToInt32(Console.ReadLine()); // Convertendo de string para Inteiro
+
+int d = n * 2;
+
+Console.WriteLine("O dobro de "+n+" é "+d);
--- exercicios/AmbienteM01/M01A08C/Program.cs
+++ exercicios/AmbienteM01/M01A08C/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // LENDO NÚMEROS PELO TECLADO EM C#
-
+Console.Write("Digite um número: ");
 int n=Convert.ToInt32(Console.ReadLine());
 
 int d = n * 2;
