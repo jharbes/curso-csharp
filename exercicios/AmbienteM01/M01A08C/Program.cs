63da5f23c306c447a8b47650ab7d787001a27f93
Jorge Harbes <jharbes@hotmail.com>
2022-12-04 15:52:09 -0300

    modulo 01 aula 08 C inicio

---
+++ exercicios/AmbienteM01/M01A08C/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// LENDO NÚMEROS PELO TECLADO EM C#
+
+int n=Convert.ToInt32(Console.ReadLine());
+
+int d = n * 2;
+
+Console.WriteLine("O dobro de "+n+" é "+d);
