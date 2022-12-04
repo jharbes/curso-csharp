2984415a9a2ada3ab37a5fa4eef5260a7ed3e0e2
Jorge Harbes <jharbes@hotmail.com>
2022-12-04 15:48:47 -0300

    entrada de dados com C#

---
+++ 03-entradaDeDados-CSharp.cs
@@ -0,0 +1,9 @@
+/*
+ * LEITURA DE DADOS VIA TECLADO EM C#
+ */
+
+string nome;
+Console.Write("Qual é o seu nome? ");
+nome = Console.ReadLine(); // Comando para ler uma linha em string no teclado
+
+Console.WriteLine("Muito prazer em te conhecer "+nome);
