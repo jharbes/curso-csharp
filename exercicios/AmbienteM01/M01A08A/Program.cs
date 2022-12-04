9de02eb9ad28d03b7a167772e4c9635a800016bc
Jorge Harbes <jharbes@hotmail.com>
2022-12-04 15:21:53 -0300

    modulo 01 aula 08 A commit 02

--- exercicios/AmbienteM01/M01A08A/Program.cs
+++ exercicios/AmbienteM01/M01A08A/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string nome;
 
+/*
+ * LEITURA DE DADOS VIA TECLADO EM C#
+ */
+
+string nome;
+Console.Write("Qual é o seu nome? ");
+nome = Console.ReadLine(); // Comando para ler uma linha em string no teclado
+
+Console.WriteLine("Muito prazer em te conhecer "+nome);
