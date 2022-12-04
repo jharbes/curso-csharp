91e0d1b57f33f816874fb747aabbdc5185a41df4
Jorge Harbes <jharbes@hotmail.com>
2022-12-04 17:15:35 -0300

    modulo 01 aula 08 E inicio

---
+++ exercicios/AmbienteM01/M01A08E/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n = 0;
+
+Console.Write("Digite um número: ");
+int.TryParse(Console.ReadLine(), out n); // Aqui pedimos que o sistema "tente" converter, caso nao seja possível ele nao finalizará a operação.
+
+int d = n * 2;
+
+Console.WriteLine("O dobro de "+n+" é "+d);
