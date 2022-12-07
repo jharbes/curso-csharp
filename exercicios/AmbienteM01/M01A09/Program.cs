7f6ab96a4efe3bde92b85f04d43882948379613c
Jorge Harbes <jharbes@hotmail.com>
2022-12-07 09:59:39 -0300

    inicio modulo 01 aula 09

---
+++ exercicios/AmbienteM01/M01A09/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// NUMEROS ALEATORIOS EM C#
+
+
+Random gerador = new Random();
+
+int n = gerador.Next();
+
+Console.WriteLine("Acabei de gerar o numero aleatorio "+n);
