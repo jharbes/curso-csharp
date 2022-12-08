3f6160fc38c1b4e6b8aaf863d7e0788e5fb7d027
Jorge Harbes <jharbes@hotmail.com>
2022-12-08 11:42:05 -0300

    modulo 01 aula 10 commit 02

--- exercicios/AmbienteM01/M01A10/Program.cs
+++ exercicios/AmbienteM01/M01A10/Program.cs
@@ -2,5 +2,21 @@
 
 
 // DATA E HORA EM C#
-Console.WriteLine("DATA E HORA EM C#");
+Console.WriteLine("DATA E HORA EM C#\n");
 
+int ano=DateTime.Now.Year;
+
+int mes=DateTime.Now.Month;
+
+int dia=DateTime.Now.Day;
+
+Console.WriteLine("A data atual é dia "+dia+" do mês "+mes+" do ano "+ano);
+
+
+int hora=DateTime.Now.Hour;
+
+int minuto=DateTime.Now.Minute;
+
+int segundo=DateTime.Now.Second;
+
+Console.WriteLine("\nA hora atual é " + hora + " horas, " + minuto + " minutos e " + segundo + " segundos.\n");
