6c27b387795d73e826715eba7d1a9e9ab68735cb
Jorge Harbes <jharbes@hotmail.com>
2022-12-12 13:31:48 -0300

    fim modulo 01 aula 13

--- 07-stringsInterpoladas-CSharp.cs
+++ 07-stringsInterpoladas-CSharp.cs
@@ -12,3 +12,15 @@ Console.WriteLine($"Olá {nome}! Tudo bem?");
 
 // Caso voce nao queira que interpole utilize o @ no lugar do $:
 
+
+// INTERPOLACAO COM NUMEROS
+
+float sal = 0;
+
+Console.Write("Qual é o seu salário? ");
+float.TryParse(Console.ReadLine(), out sal);
+
+Console.WriteLine($"Você ganha {sal} por mês!");
+
+Console.WriteLine($"Você ganha {sal:C} por mês!"); // O :C ja formata o valor para moeda corrente local, ou seja , em reais,   porque o sistema no caso está localizado no Brasil, se fosse localizado nos EUA a saida seria em Dólar.
+
--- exercicios/AmbienteM01/M01A12C/Program.cs
+++ exercicios/AmbienteM01/M01A12C/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// INTERPOLACAO COM NUMEROS
+
+float sal = 0;
+
+Console.Write("Qual é o seu salário? ");
+float.TryParse(Console.ReadLine(), out sal);
+
+Console.WriteLine($"Você ganha {sal} por mês!");
+
+Console.WriteLine($"Você ganha {sal:C} por mês!"); // O :C ja formata o valor para moeda corrente local, ou seja , em reais,   porque o sistema no caso está localizado no Brasil, se fosse localizado nos EUA a saida seria em Dólar.
+
