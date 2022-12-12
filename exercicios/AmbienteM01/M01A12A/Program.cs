34939ffbad7c6bcdee210841a243120b16bdbded
Jorge Harbes <jharbes@hotmail.com>
2022-12-12 13:04:50 -0300

    inicio modulo 01 aula 12 A

---
+++ exercicios/AmbienteM01/M01A12A/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// STRING INTERPOLADAS EM C#
+
+Console.Write("Digite seu nome: ");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Olá "+nome+"! Tudo bem?");
+
+// EXEMPLO com interpolacao:
+
+Console.WriteLine($"Olá {nome}! Tudo bem?");
+
+
+// Caso voce nao queira que interpole utilize o @ no lugar do $:
+
