3fa74b90b951a89bf94ec38d8c9b502146e5af4c
Jorge Harbes <jharbes@hotmail.com>
2022-12-16 07:21:21 -0300

    modulo 01 aula 13 A commit 02

--- exercicios/AmbienteM01/M01A13A/Program.cs
+++ exercicios/AmbienteM01/M01A13A/Program.cs
@@ -4,3 +4,6 @@
 
 
 string nome = "Gustavo";
+Console.WriteLine($"Muito prazer em te conhecer {nome,20}!!!"); // Mostrar o nome usando 20 letras. (Cria um espaço na frente para completar o numero de letras, alinhando a variavel a direita, para alinhar a esquerda basta colocar um numero negativo conforme exemplo abaixo)
+Console.WriteLine($"\nMuito prazer em te conhecer {nome,-20}!!!");
+
