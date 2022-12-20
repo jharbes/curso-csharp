e6bc3b0cda66ad822a9221d998229e5d72098f37
Jorge Harbes <jharbes@hotmail.com>
2022-12-20 17:11:23 -0300

    ex001 modulo 01 finalizado

--- exercicios/ExerciciosM01/Ex001/Program.cs
+++ exercicios/ExerciciosM01/Ex001/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Inicio exercicios Modulo 01");
+
+
+Console.SetCursorPosition(10, 3);
+
+Console.ForegroundColor= ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.Write(" MEU ");
+System.Threading.Thread.Sleep(1000);
+
+Console.ForegroundColor=ConsoleColor.Green;
+Console.BackgroundColor= ConsoleColor.Yellow;
+Console.Write(" BRASIL ");
+System.Threading.Thread.Sleep(1000);
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor=ConsoleColor.Green;
+Console.Write(" BRASILEIRO ");
+System.Threading.Thread.Sleep(1000);
+
+Console.ResetColor();
