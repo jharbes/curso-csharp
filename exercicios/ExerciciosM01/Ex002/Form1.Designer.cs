db6be5a531a468026faf53b05def5d5898ce5d60
Jorge Harbes <jharbes@hotmail.com>
2022-12-24 09:31:29 -0300

    Ex002 M01 commit 03 minha versao

--- exercicios/ExerciciosM01/Ex002/Form1.Designer.cs
+++ exercicios/ExerciciosM01/Ex002/Form1.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.btnIniciar = new System.Windows.Forms.Button();
             this.lblMsg = new System.Windows.Forms.Label();
+            this.lblMsg2 = new System.Windows.Forms.Label();
+            this.lblMsg3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnIniciar
             // 
-            this.btnIniciar.Location = new System.Drawing.Point(140, 181);
+            this.btnIniciar.Location = new System.Drawing.Point(173, 186);
             this.btnIniciar.Name = "btnIniciar";
             this.btnIniciar.Size = new System.Drawing.Size(104, 37);
             this.btnIniciar.TabIndex = 0;
@@ -46,23 +48,47 @@
             // 
             this.lblMsg.AutoSize = true;
             this.lblMsg.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblMsg.Location = new System.Drawing.Point(-3, 88);
+            this.lblMsg.Location = new System.Drawing.Point(16, 88);
             this.lblMsg.Name = "lblMsg";
-            this.lblMsg.Size = new System.Drawing.Size(384, 40);
+            this.lblMsg.Size = new System.Drawing.Size(94, 40);
             this.lblMsg.TabIndex = 1;
-            this.lblMsg.Text = " MEU   BRASIL   BRASILEIRO ";
+            this.lblMsg.Text = " MEU ";
+            // 
+            // lblMsg2
+            // 
+            this.lblMsg2.AutoSize = true;
+            this.lblMsg2.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMsg2.Location = new System.Drawing.Point(116, 88);
+            this.lblMsg2.Name = "lblMsg2";
+            this.lblMsg2.Size = new System.Drawing.Size(123, 40);
+            this.lblMsg2.TabIndex = 2;
+            this.lblMsg2.Text = " BRASIL ";
+            // 
+            // lblMsg3
+            // 
+            this.lblMsg3.AutoSize = true;
+            this.lblMsg3.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMsg3.Location = new System.Drawing.Point(245, 88);
+            this.lblMsg3.Name = "lblMsg3";
+            this.lblMsg3.Size = new System.Drawing.Size(185, 40);
+            this.lblMsg3.TabIndex = 3;
+            this.lblMsg3.Text = " BRASILEIRO ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(376, 294);
+            this.ClientSize = new System.Drawing.Size(442, 294);
+            this.Controls.Add(this.lblMsg3);
+            this.Controls.Add(this.lblMsg2);
             this.Controls.Add(this.lblMsg);
             this.Controls.Add(this.btnIniciar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Brasil";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,5 +98,7 @@
 
         private Button btnIniciar;
         private Label lblMsg;
+        private Label lblMsg2;
+        private Label lblMsg3;
     }
 }
--- exercicios/ExerciciosM01/Ex002/Form1.cs
+++ exercicios/ExerciciosM01/Ex002/Form1.cs
@@ -7,9 +7,30 @@ namespace Ex002
             InitializeComponent();
         }
 
-        private void btnIniciar_Click(object sender, EventArgs e)
+        private async void btnIniciar_Click(object sender, EventArgs e)
         {
             lblMsg.Visible = true;
+            lblMsg2.Visible = true;
+            lblMsg3.Visible = true;
+
+            await Task.Delay(2000);
+
+            lblMsg.BackColor = Color.Blue;
+            lblMsg.ForeColor = Color.White;
+
+            await Task.Delay(2000);
+
+            lblMsg2.BackColor = Color.Yellow;
+            lblMsg2.ForeColor = Color.Green;
+
+            await Task.Delay(2000);
+
+            lblMsg3.BackColor = Color.Green;
+            lblMsg3.ForeColor = Color.Yellow;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
         }
     }
