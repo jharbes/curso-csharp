747293de2c8f13d3db5ec3eb44d203d98f2f9126
Jorge Harbes <jharbes@hotmail.com>
2022-12-26 16:14:35 -0300

    Ex003 M01 inicio

---
+++ exercicios/ExerciciosM01/Ex003/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Digite um número real: ");
+float num = 0;
+float.TryParse(Console.ReadLine(), out num);
+
+Console.WriteLine(num);
